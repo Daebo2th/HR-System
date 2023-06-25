ceefc5dbc51a0d371ab3be7b850a27b1aea50741
Song014 <ghgsb6200@gmail.com>
2023-06-26 08:58:21 +0900

    test fix

--- src/main/java/com/kosa/hrsystem/HelloServlet.java
+++ src/main/java/com/kosa/hrsystem/HelloServlet.java
@@ -14,7 +14,6 @@ public class HelloServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-qweqwe
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
